49482efe574eae5511fb1d9d80c472352c5cbd7e
bacosta4 <bacosta4@miners.utep.edu>
2020-10-29 22:37:10 -0600

    Error with make all 'command not available'

---
+++ project/srcc/stateMachines.c
@@ -0,0 +1,36 @@
+#include <msp430.h>
+#include "stateMachines.h"
+#include "led.h"
+#include "switches.h"
+
+char toggle_red()		/* always red led! */
+{
+  red_on = 1;
+  led_changed = 1;
+  led_update();
+}
+
+char toggle_green()	/* always green led!  */
+{
+  green_on = 1;
+  led_changed = 1;
+  led_update();
+}
+
+void toggle_led()
+{
+  static char state = 0;
+  
+  switch(state) {
+  case 0:
+
+    toggle_red();
+    state = 1;
+    break;
+
+  case 1:
+    toggle_green();
+    state = 0;
+    break;
+  }
+}
--- project/srcc/switches.c
+++ project/srcc/switches.c
@@ -1,13 +1,18 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
+#include "stateMachines.h"
 
-char switch_state_down, switch_state_changed; /* effectively boolean */
+//char switch_state_down, switch_state_changed; /* effectively boolean */
+
+char sw1_state_down,sw2_state_down,sw3_state_down,sw4_state_down;
+int switch_state = 0;
+
 
 static char 
 switch_update_interrupt_sense()
 {
-  char p1val = P2IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
   P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
   P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
@@ -29,8 +34,22 @@ void
 switch_interrupt_handler()
 {
   char p2val = switch_update_interrupt_sense();
-  //switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
-  //switch_state_changed = 1;
-  //led_update();
+
+  switch1_state = (p2val & SW1) ? 0 : 1;  // which button is being pressed
+  switch2_state = (p2val & SW2) ? 0 : 1;
+  switch3_state = (p2val & SW3) ? 0 : 1;
+  switch4_state = (p2val & SW4) ? 0 : 1;
+
+  if (switch1_state) {   // which button was pressed
+    switch_state = 1;
+    toggle_led();
+  }
+  
+  if (switch2_state)
+    switch_state = 2;
+  if (switch3_state)
+    switch_state = 3;
+  if (switch4_state)
+    switch_state = 4;
  
 }
